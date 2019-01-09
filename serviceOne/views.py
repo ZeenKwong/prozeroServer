4e4bd80326cc695656c2acd5da9cf0660f74f277
邝知研 <kuangzy@chinaunicom.cn>
2019-01-09 21:40:50 +0800

    原来html要备案的

--- serviceOne/views.py
+++ serviceOne/views.py
@@ -7,4 +7,5 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def index(request):
     # return JsonResponse({"result": 0, "msg": "success"})
-    return render(request, 'test1/htmlTest.html')
+    return JsonResponse({'say':'miss me'})
+    # return render(request, 'test1/htmlTest.html')
